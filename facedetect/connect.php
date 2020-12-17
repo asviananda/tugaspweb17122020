f0714a0a0138ddc210de44f210045112adf24f89
asviananda <62512432+asviananda@users.noreply.github.com>
2020-12-17 22:10:20 +0700

    Create connect.php

---
+++ facedetect/connect.php
@@ -0,0 +1,10 @@
+<?php 
+
+$databaseHost = "localhost";
+$databaseName="uploadb";
+$databaseUsername="root";
+$databasePassword="";
+
+$mysqli=mysqli_connect($databaseHost,$databaseUsername,$databasePassword,$databaseName);
+
+?>
